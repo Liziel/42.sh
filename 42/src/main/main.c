86ef3a4b753071d83d7f3586aefb1fa6d4017912
ColliotV <collio_v@epitech.eu>
2013-04-29 19:46:56 +0200

    ok

---
+++ 42/src/main/main.c
@@ -0,0 +1,9 @@
+/*
+** main.c for  in /home/collio_v/rendu_svn/42.sh/42
+**
+** Made by vincent colliot
+** Login   <collio_v@epitech.net>
+**
+** Started on  Sat Apr 27 09:09:51 2013 vincent colliot
+** Last update Mon Apr 29 19:46:36 2013 vincent colliot
+*/
