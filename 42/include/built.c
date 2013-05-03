38d79d10ab20f51710fa9b2428780e95b41ccff8
my-get-dodo <thomas.lecorre@epitech.eu>
2013-05-03 18:10:07 +0200

    coucou

---
+++ 42/include/built.c
@@ -0,0 +1,16 @@
+/*
+** built.c for 42 in /home/lecorr_b//projets/42sh/42.sh/42/include
+**
+** Made by thomas lecorre
+** Login   <lecorr_b@epitech.net>
+**
+** Started on  Fri May  3 17:56:39 2013 thomas lecorre
+** Last update Fri May  3 17:57:18 2013 thomas lecorre
+*/
+
+#ifndef	BUILT_H__
+#define BUILT_H__
+
+
+
+#endif
---
+++ 42/src/builtins/parse_built.c
@@ -0,0 +1,10 @@
+/*
+** parse_built.c for  in /home/lecorr_b//projets/42sh/42.sh/42/src/builtins
+**
+** Made by thomas lecorre
+** Login   <lecorr_b@epitech.net>
+**
+** Started on  Fri May  3 17:56:18 2013 thomas lecorre
+** Last update Fri May  3 17:59:55 2013 thomas lecorre
+*/
+
