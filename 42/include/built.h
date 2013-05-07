c9a34d4a940e907bc0e3ea66b1bb4c9996c35c92
my-get-dodo <thomas.lecorre@epitech.eu>
2013-05-07 10:27:31 +0200

    c

--- 42/include/built.h
+++ 42/include/built.h
@@ -5,13 +5,14 @@
 ** Login   <lecorr_b@epitech.net>
 **
 ** Started on  Fri May  3 17:56:39 2013 thomas lecorre
-** Last update Mon May  6 16:57:08 2013 vincent colliot
+** Last update Tue May  7 10:27:18 2013 thomas lecorre
 */
 
 #ifndef	BUILT_H__
 #define BUILT_H__
 
-typedef struct s_call{
+typedef struct s_call
+{
   char	*name;
   //ptr int function (t_words*)
 }
