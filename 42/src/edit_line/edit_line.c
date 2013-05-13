09c7de2a4f9482d6fb634a95feaa9762e2a73101
pierre-yves thomas <thomas_1@pc-thomas_1.(none)>
2013-05-13 13:48:13 +0200

    Assemblage edit_line

---
+++ 42/src/edit_line/edit_line.c
@@ -0,0 +1,56 @@
+/*
+** edit_line.c for 42 in /home/thomas_1//Projets/42sh
+** 
+** Made by pierre-yves thomas
+** Login   <thomas_1@epitech.net>
+** 
+** Started on  Fri Apr 26 14:36:25 2013 pierre-yves thomas
+** Last update Mon May 13 13:36:48 2013 pierre-yves thomas
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "edit_line.h"
+
+int		init_values(int *history_pl, int **rev_c,
+			    char **str, char **cmd)
+{
+  *history_pl = 0;
+  if (((*str) = malloc(sizeof(**str) * 5)) == NULL ||
+      ((*cmd) = malloc(sizeof(**cmd) * 2)) == NULL ||
+      ((*rev_c) = malloc(sizeof(**rev_c) * 2)) == NULL ||)
+    {
+      if ((*str) != NULL)
+	free(*str);
+      return (-1);
+    }
+  (*str) = memset((*str), 0, 5);
+  (*cmd) = memset((*cmd), 0, 2);
+  (*rev_c)[0] = 0;
+  return (0);
+}
+
+char		*usr_cmd(int fd, t_options options)
+{
+  char		*str;
+  char		*cmd;
+  int		*reverse_case;
+  int		history_pl;
+
+  if (init_values(&history_pl, &reverse_case, &str, &cmd) == -1)
+    return (NULL);
+  reverse_case[1] = fd
+  show_cmd(str[0], cmd, reverse_case, options);
+  while (str[0] != 10 || str[1] != 0 || str[2] != 0)
+    {
+      str = memset(str, 0, 5);
+      if (read(fd, str, 4) <= 0 || str[0] == 4)
+	return (NULL);
+      modif_cmd(&cmd, str, &reverse_case[0], options);
+      show_cmd(str[0], cmd, reverse_case, options);
+    }
+  my_putstr(1, "\n");
+  free(str);
+  return (cmd);
+}
---
+++ 42/src/edit_line/modify_line.c
@@ -0,0 +1,74 @@
+/*
+** modify_line.c for 42sh in /home/thomas_1//Projets/42sh/src
+** 
+** Made by pierre-yves thomas
+** Login   <thomas_1@epitech.net>
+** 
+** Started on  Mon May  6 17:44:08 2013 pierre-yves thomas
+** Last update Mon May 13 13:20:05 2013 pierre-yves thomas
+*/
+
+#include <string.h>
+#include <stdlib.h>
+#include "edit_line.h"
+
+void    move_reverse_case(char value, int *reverse_case, char *cmd)
+{
+  int   len;
+
+  len = strlen(cmd) - 1;
+  if (value == 68 && (*reverse_case) > 0)
+    (*reverse_case)--;
+  else if (value == 67 && (*reverse_case) <= len)
+    (*reverse_case)++;
+}
+
+
+void    add_letter_in_str(char value, char **cmd, int *reverse_case)
+{
+  int   len;
+
+  len = strlen(*cmd) + 1;
+  while (--len > (*reverse_case))
+    (*cmd)[len + 1] = (*cmd)[len];
+  if (strlen(*cmd) + 1 > 1)
+    (*cmd)[len + 1] = (*cmd)[len];
+  (*cmd)[len] = value;
+  (*reverse_case)++;
+  if (((*cmd) = realloc(*cmd, strlen(*cmd) + 2)) != NULL)
+    (*cmd)[strlen(*cmd) + 1] = '\0';
+}
+
+void    del_letter_in_str(int *reverse_case, char **cmd)
+{
+  int   suppr;
+
+  suppr = (*reverse_case) - 1;
+  if ((*reverse_case) > 0)
+    {
+      while ((*cmd)[suppr] != '\0')
+        {
+          (*cmd)[suppr] = (*cmd)[suppr + 1];
+          suppr++;
+        }
+      (*reverse_case)--;
+    }
+}
+
+void            modif_cmd(char **cmd, char *str,
+                          int *reverse_case, t_options options)
+{
+  int		i;
+
+  i = -1;
+  if (str[0] == 27 && (str[2] == 68 || str[2] == 67))
+    move_reverse_case(str[2], reverse_case, *cmd);
+  else if (str[0] == 127 && str[1] == 0)
+    del_letter_in_str(reverse_case, cmd);
+  else if (str[0] == 1)
+    (*reverse_case) = 0;
+  else if (str[0] == 5)
+    (*reverse_case) = strlen(*cmd);
+  while (++i < 4 && str[i] != 0 && str[i] != 10 && str[i] != 27)
+    add_letter_in_str(str[i], cmd, reverse_case);
+}
