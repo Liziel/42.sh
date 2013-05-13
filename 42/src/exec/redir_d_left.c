2fe44a9486003927d09bc053528eb44e71603dee
ColliotV <collio_v@epitech.eu>
2013-05-13 12:58:06 +0200

    git commit -a (git commit -a)

--- 42/src/exec/redir_d_left.c
+++ 42/src/exec/redir_d_left.c
@@ -5,15 +5,32 @@
 ** Login   <collio_v@epitech.net>
 **
 ** Started on  Mon May 13 00:38:40 2013 vincent colliot
-** Last update Mon May 13 02:08:14 2013 vincent colliot
+** Last update Mon May 13 02:27:59 2013 vincent colliot
 */
 
 BOOL	rdleft(t_redir *r, FD w[3])
 {
+  t_words	*lines;
+  pid_t	pid;
   FD	p[2];
 
-  //open tty4
-  //fork()
-  //->fils écrit sur pipe
-  //père continue son bout de chemin
+  if ((p[0] = open("/dev/tty4", O_RDONLY)) == -1)
+    {
+      put_err("(sh): can't open tty4\n");
+      return (TRUE);
+    }
+  //lines = get_alls(p[0]);
+  close(p[0]);
+  pipes(p);
+  if ((pid = xfork()) == -1)
+    return (FALSE);
+  if (!pid)
+    {
+      close(p[W_IN]);
+      return (put_lines(lines, w[P_OUT]));
+    }
+  close(p[W_OUT]);
+  w[r->in] = p[W_IN];
+  nullify_cmd_words(lines);
+  return (TRUE);
 }
