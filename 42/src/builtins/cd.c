8f78bfee8e737c8ccfc5df78ca5fc97ecb9493d8
quentin cloarec <cloare_q@pc-cloare_q.(none)>
2013-05-13 15:14:45 +0200

    cd

--- 42/src/builtins/cd.c
+++ 42/src/builtins/cd.c
@@ -5,7 +5,7 @@
 ** Login   <cloare_q@epitech.net>
 ** 
 ** Started on  Mon May 13 11:58:06 2013 quentin cloarec
-** Last update Mon May 13 14:52:02 2013 quentin cloarec
+** Last update Mon May 13 15:14:09 2013 quentin cloarec
 */
 
 #include <unistd.h>
@@ -23,10 +23,11 @@ char	*put_pwd(char *s)
   j = 0;
   if ((pwd = xmalloc(sizeof(char) * (my_strlen(s) + 5))) == NULL)
     return (EXIT_FAILURE);
-  pwd[0] = 'P';
-  pwd[1] = 'W';
-  pwd[2] = 'D';
-  i = 3;
+  pwd[0] = 'T';
+  pwd[1] = 'M';
+  pwd[2] = 'P';
+  pwd[3] = ' ';
+  i = 4;
   while (s[j])
     pwd[i++] = s[j++];
   return (pwd);
@@ -36,55 +37,41 @@ int	just_cd(t_words *cmd)
 {
   if ((chdir(get_env("HOME"))) == -1)
     {
-      my_putstr("CHDIR ERROR\n");
+      print_err("CHDIR ERROR\n");
       return (EXIT_FAILURE);
     }
 }
 
 int	cd(t_words *cmd, void *alias)
 {
-  static char *pwd;
-  static int	i;  
+  int	i;  
   char	*str;
 
-  if (i == 0)
-    {
-      if ((pwd = xmalloc(sizeof(char) * (my_strlen(get_env("PWD")) + 1))) == NULL)
-	return (EXIT_FAILURE);
-      pwd = get_env("PWD");
-    }
   cmd = cmd->next;
   if (MATCH(cmd->word, "cd") == 1)
     {
       just_cd(cmd);
       str = put_pwd(get_env("HOME"));
-      put_env(str);
-      if ((pwd = xmalloc(sizeof(char) * (my_strlen(get_env("PWD")) + 1))) == NULL)
-	return (EXIT_FAILURE);
-      pwd = get_env("PWD");
-      i++;
+      add_env(str);
     }
   else if (MATCH(cmd->word, "cd -") == 1)
     {
-      if ((chdir(get_env("PWD"))) == -1)
+      if ((chdir(get_env("TMP"))) == -1)
 	{
-	  my_putstr("CHDIR ERROR\n");
+	  print_err("CHDIR ERROR\n");
 	  return (EXIT_FAILURE);
 	}
-      if ((pwd = xmalloc(sizeof(char) * (my_strlen(get_env("PWD")) + 1))) == NULL)
-        return (EXIT_FAILURE);
-      pwd = get_env("PWD");
-      i++;
+      str = put_pwd(get_env("PWD"));
+      add_env(str);
     }
   else
     {
       if ((chdir(cmd->word)) == -1)
 	{
-          my_putstr("CHDIR ERROR\n");
+          print_err("CHDIR ERROR\n");
           return (EXIT_FAILURE);
         }
-      if ((pwd = xmalloc(sizeof(char) * (my_strlen(get_env("PWD")) + 1))) == NULL)
-        return (EXIT_FAILURE);
-      pwd = get_env("PWD");
+      str = put_pwd(cmd->word);
+      add_env(str);
     }
 }
