ac20b4c714f07f89f32ad4bd6e46da2ccc71e6d5
ColliotV <collio_v@epitech.eu>
2013-05-20 16:50:40 +0200

    false

--- 42/src/builtins/call/parse_built.c
+++ 42/src/builtins/call/parse_built.c
@@ -5,29 +5,31 @@
 ** Login   <lecorr_b@epitech.net>
 **
 ** Started on  Fri May  3 17:56:18 2013 thomas lecorre
-** Last update Tue May 14 18:10:39 2013 thomas lecorre
+** Last update Sun May 19 08:43:49 2013 vincent colliot
 */
 
 #include "built.h"
 
-void	init_built(t_call built[5])
+void	init_built(t_call built[9])
 {
-  (built[0]).name = my_strdup("cd");
-  (built[0]).ptr = &cd;
-  (built[1]).name = my_strdup("echo");
-  (built[1]).ptr = &my_echo;
-  (built[2]).name = my_strdup("exit");
-  (built[2]).ptr = &my_exit;
-  (built[3]).name = my_strdup("setenv");
-  (built[3]).ptr = &setenv;
-  (built[4]).name = my_strdup("unsetenv");
-  (built[4]).ptr = &unsetenv;
-  (built[5]).name = my_strdup("alias");
-  (built[5]).ptr = &alias;
-  (built[5]).name = my_strdup("unalias");
-  (built[5]).ptr = &unalias;
-  (built[6]).name = NULL;
-  (built[6]).ptr = NULL;
+  (built[0]).name = "alias";
+  (built[0]).ptr = &built_alias;
+  (built[1]).name = "cd";
+  (built[1]).ptr = &built_cd;
+  (built[2]).name = "echo";
+  (built[2]).ptr = &built_echo;
+  (built[3]).name = "env";
+  (built[3]).ptr = &built_env;
+  (built[4]).name = "exit";
+  (built[4]).ptr = &my_exit;
+  (built[5]).name = "setenv";
+  (built[5]).ptr = &setenv;
+  (built[6]).name = "unsetenv";
+  (built[6]).ptr = &unsetenv;
+  (built[7]).name = "unalias";
+  (built[7]).ptr = &unalias;
+  (built[8]).name = NULL;
+  (built[8]).ptr = NULL;
 }
 
 void	*stock_tab(t_call *built)
@@ -43,7 +45,7 @@ int	builtins(t_words *cmd, void *alias)
 {
   t_call	*built;
 
-  if ((built = xmalloc(sizeof(t_call) * 7)) == NULL)
+  if ((built = xmalloc(sizeof(t_call) * 9)) == NULL)
     return (-1);
   init_built(&built);
   stock_tab(&built);
