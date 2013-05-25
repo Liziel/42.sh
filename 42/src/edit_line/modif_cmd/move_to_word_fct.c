93eafff5750c6460d7b7a12e720e455d9e87e7cf
ColliotV <collio_v@epitech.eu>
2013-05-26 01:56:05 +0200

    yep

--- 42/src/edit_line/modif_cmd/move_to_word_fct.c
+++ 42/src/edit_line/modif_cmd/move_to_word_fct.c
@@ -5,20 +5,15 @@
 ** Login   <thomas_1@epitech.net>
 **
 ** Started on  Fri May 10 13:07:12 2013 pierre-yves thomas
-<<<<<<< HEAD
-** Last update Sun May 26 01:45:40 2013 vincent colliot
-=======
+** Last update Sun May 26 01:55:55 2013 vincent colliot
 ** Last update Sun May 26 01:47:26 2013 thomas lecorre
->>>>>>> 40282a65d26cf41ee56033bebac17173e54044a9
 */
 
 #include "string.h"
 
 void	move_to_prev_word(char c, int *rev_c, char **cmd)
 {
-  if (*cmd == NULL)
-    return ;
-
+  (void)c;
   if (*rev_c != 0 && (*cmd)[*rev_c] != ' ' && (*cmd)[*rev_c] != '\t')
     (*rev_c)--;
   while (*rev_c != 0 && ((*cmd)[*rev_c] == ' ' || (*cmd)[*rev_c] == '\t'))
@@ -33,6 +28,7 @@ void	move_to_next_word(char c, int *rev_c, char **cmd)
 {
   int	len;
 
+  (void)c;
   len = my_strlen(*cmd);
   if (*rev_c < len && (*cmd)[*rev_c] != ' ' && (*cmd)[*rev_c] != '\t')
     (*rev_c)++;
